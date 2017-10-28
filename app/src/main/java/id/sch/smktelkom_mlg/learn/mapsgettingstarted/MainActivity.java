8d45c61302e4605a680b2dbd2bbe9e23e58f8a16
karinawidhia <karina_nugrahini_24rpl@student.smktelkom-mlg.sch.id>
2017-10-28 14:32:32 +0700

    -Camera Move

--- app/src/main/java/id/sch/smktelkom_mlg/learn/mapsgettingstarted/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/mapsgettingstarted/MainActivity.java
@@ -2,6 +2,8 @@ package id.sch.smktelkom_mlg.learn.mapsgettingstarted;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -15,6 +17,30 @@ import com.google.android.gms.maps.model.LatLng;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    static final CameraPosition SEATTLE = CameraPosition.builder()
+            .target(new LatLng(47.6204, -122.3491))
+            .zoom(17)
+            .bearing(0)
+            .tilt(45)
+            .build();
+    static final CameraPosition NEW_YORK = CameraPosition.builder()
+            .target(new LatLng(40.712775, -74.005973))
+            .zoom(17)
+            .bearing(0)
+            .tilt(45)
+            .build();
+    static final CameraPosition DUBLIN = CameraPosition.builder()
+            .target(new LatLng(53.3478, 6.2597))
+            .zoom(17)
+            .bearing(90)
+            .tilt(45)
+            .build();
+    static final CameraPosition JKT = CameraPosition.builder()
+            .target(new LatLng(-6.175110, 106.865039))
+            .zoom(17)
+            .bearing(90)
+            .tilt(45)
+            .build();
     GoogleMap m_map;
     Boolean mapReady = false;
     Button bMaps, bSat, bHy;
@@ -32,7 +58,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             @Override
             public void onClick(View view) {
                 if (mapReady)
-                    m_map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                    flyTo(SEATTLE);
             }
         });
 
@@ -40,7 +66,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             @Override
             public void onClick(View view) {
                 if (mapReady)
-                    m_map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                    //m_map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                    flyTo(NEW_YORK);
             }
         });
 
@@ -48,7 +75,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             @Override
             public void onClick(View view) {
                 if (mapReady)
-                    m_map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                    flyTo(JKT);
+//                    m_map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
             }
         });
 
@@ -57,12 +85,39 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void flyTo(CameraPosition target) {
+        m_map.animateCamera(CameraUpdateFactory.newCameraPosition(target), 10000, null);
+    }
+
+    @Override
     public void onMapReady(GoogleMap googleMap) {
         Toast toast = Toast.makeText(getApplicationContext(), "Map Ready!", Toast.LENGTH_SHORT);
         toast.show();
         mapReady = true;
         m_map = googleMap;
-        LatLng nganjuk = new LatLng(-7.601427, 111.901216);
+        LatLng nganjuk = new LatLng(53.3478, 6.2597);
+        m_map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
         CameraPosition target = CameraPosition.builder().target(nganjuk).zoom(14).build();
         m_map.moveCamera(CameraUpdateFactory.newCameraPosition(target));
     }
